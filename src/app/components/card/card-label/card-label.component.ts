dafe038d5e062a49994856f43891d3bb23be2b6a
victot-exe <victorfarian@gmail.com>
2023-10-05 22:45:25 -0300

    finished

--- src/app/components/card/card-label/card-label.component.ts
+++ src/app/components/card/card-label/card-label.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CardLabelComponent implements OnInit {
 	@Input()
-	gameLabel: string=""
+	gameLabel: string=''
 
   constructor() { }
 
